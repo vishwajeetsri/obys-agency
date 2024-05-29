030aa3372af973cf43aa40b748913f01dda88f26
vishwajeetsri <vs0397297@gmail.com>
2024-05-29 17:50:52 +0530

    page1 done

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-
-
-
+function loadingAnimation (){
+    
 var tl = gsap.timeline()
 tl.from(".line h1",{
     y:150,
@@ -28,7 +27,7 @@ setInterval(function(){
 tl.to(".line h2",{
     opacity:1,
     animationName:"anime",
-    
+
 })
 
 tl.to("#loader",{
@@ -48,3 +47,32 @@ tl.from("#page1",{
 tl.to("#loader",{
     display:"none"
 })
+
+tl.from("#nav",{
+    opacity:0
+})
+
+tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1",{
+    y:120,
+    stagger:0.2
+})
+}
+function CursorAnimtion(){
+    document.addEventListener("mousemove",function(dets){
+        gsap.to("#cursr",{
+            left:dets.x,
+            top:dets.y
+        })
+    })
+    
+    Shery.makeMagnet("#nav-part2 h4");
+}
+loadingAnimation();
+CursorAnimtion();
+
+
+
+
+
+
+
